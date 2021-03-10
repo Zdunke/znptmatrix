318fbc50d4ba51ff0afd338b8e815ee6aa3d52cf
Zdunke <zdun.piotr@gmail.com>
2021-03-10 11:20:23 +0100

    indexers added

--- MatrixTests/MatrixTests.cs
+++ MatrixTests/MatrixTests.cs
@@ -14,17 +14,34 @@ namespace ZnptMatrix
       _b = new Matrix<double>(new[,] { { 1d, 2d, 3d }, { 4d, 5d, 6d } });
     }
     [Fact]
-    public void NumbersOfRows()
+    public void TestNumbersOfRows()
     {
       Assert.Equal(3, _a.NumberOfRows);
       Assert.Equal(2, _b.NumberOfRows);
     }
     [Fact]
-    public void NumberOfColumns()
+    public void TestNumberOfColumns()
     {
       Assert.Equal(2, _a.NumberOfColumns);
       Assert.Equal(3, _b.NumberOfColumns);
     }
+    [Fact]
+    public void TestGetIndexers()
+    {
+      Assert.Equal(1, _a[0, 0]);
+      Assert.Equal(5, _a[1, 1]);
+      Assert.Throws<IndexOutOfRangeException>(() => _a[9, 9]);
+    }
+    [Fact]
+    public void TestSetIndexers()
+    {
+      _a[0, 1] = 101;
+      _a[1, 1] = 405;
+      Assert.Equal(101, _a[0, 1]);
+      Assert.Equal(405, _a[1, 1]);
+      Assert.Throws<IndexOutOfRangeException>(() => _a[9, 9] = 101);
+    }
+
   }
 }
 
